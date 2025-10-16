5402bc576a2c5380c617fc787f7e3935422c0874
mdchad <irsyad.muhd@gmail.com>
2025-10-16 11:08:05 +0800

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,10 @@
+import { drizzle } from "drizzle-orm/libsql";
+import { env } from "cloudflare:workers";
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+	url: env.DATABASE_URL || "",
+	authToken: env.DATABASE_AUTH_TOKEN,
+});
+
+export const db = drizzle({ client });
