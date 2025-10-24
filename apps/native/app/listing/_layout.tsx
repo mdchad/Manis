e78276524d6b770bad33b1825d011e8d93eabb88
mdchad <irsyad.muhd@gmail.com>
2025-10-24 16:46:28 +0800

    fix layout

--- apps/native/app/listing/_layout.tsx
+++ apps/native/app/listing/_layout.tsx
@@ -1,16 +1,21 @@
 import { Stack } from "expo-router";
+import React from "react";
 
 export default function ListingLayout() {
 	return (
 		<Stack
 			screenOptions={{
+				headerShown: false
+			}}
+		>
+			<Stack.Screen name="[id]" options={{
 				headerShown: true,
+				headerTitle: "",
 				headerShadowVisible: false,
 				headerStyle: {
 					backgroundColor: "#F2F2EA",
-				},
-				headerTitle: "OI BRUV"
-			}}
-		/>
-	);
+				}
+			}} />
+		</Stack>
+	)
 }
