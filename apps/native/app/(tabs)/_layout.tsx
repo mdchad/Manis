be437a3268a1b2b76f5be38d7f0d80bdd5b1bbd5
mdchad <irsyad.muhd@gmail.com>
2025-11-06 00:26:32 +0800

    test better auth, test integration with convex

--- apps/native/app/(tabs)/_layout.tsx
+++ apps/native/app/(tabs)/_layout.tsx
@@ -8,9 +8,10 @@ import {
 	HouseIcon,
 	MessageCircleIcon,
 	PlusIcon,
-	SearchIcon
-} from 'lucide-react-native';
-import {Image, View} from "react-native";
+	SearchIcon,
+	UserIcon,
+} from "lucide-react-native";
+import { Image, View } from "react-native";
 import React from "react";
 
 export default function TabLayout() {
@@ -27,7 +28,7 @@ export default function TabLayout() {
 				headerShadowVisible: false,
 				headerStyle: {
 					backgroundColor: "#F2F2EA",
-					borderColor: 'transparent'
+					borderColor: "transparent",
 				},
 				headerRight: () => (
 					<View className="pr-4">
@@ -36,24 +37,16 @@ export default function TabLayout() {
 				),
 				headerTitle: () => (
 					<Image
-						source={require('@/assets/images/app-header-logo.png')}
+						source={require("@/assets/images/app-header-logo.png")}
 						style={{ width: 80, height: 80 }}
 						resizeMode="contain"
 					/>
 				),
-				tabBarActiveTintColor: isDarkColorScheme
-					? "hsl(217.2 91.2% 59.8%)"
-					: "#8cb700",
-				tabBarInactiveTintColor: isDarkColorScheme
-					? "hsl(215 20.2% 65.1%)"
-					: "#e2296f",
+				tabBarActiveTintColor: isDarkColorScheme ? "hsl(217.2 91.2% 59.8%)" : "#8cb700",
+				tabBarInactiveTintColor: isDarkColorScheme ? "hsl(215 20.2% 65.1%)" : "#e2296f",
 				tabBarStyle: {
-					backgroundColor: isDarkColorScheme
-						? "hsl(222.2 84% 4.9%)"
-						: "hsl(0 0% 100%)",
-					borderTopColor: isDarkColorScheme
-						? "hsl(217.2 32.6% 17.5%)"
-						: "hsl(214.3 31.8% 91.4%)",
+					backgroundColor: isDarkColorScheme ? "hsl(222.2 84% 4.9%)" : "hsl(0 0% 100%)",
+					borderTopColor: isDarkColorScheme ? "hsl(217.2 32.6% 17.5%)" : "hsl(214.3 31.8% 91.4%)",
 				},
 			}}
 		>
@@ -61,34 +54,26 @@ export default function TabLayout() {
 				name="index"
 				options={{
 					title: "Home",
-					tabBarIcon: ({ color }) => (
-						<HouseIcon color={color} />
-					),
+					tabBarIcon: ({ color }) => <HouseIcon color={color} />,
 				}}
 			/>
 			<Tabs.Screen
 				name="two"
 				options={{
 					title: "Explore",
-					tabBarIcon: ({ color }) => (
-						<SearchIcon color={color} />
-					),
+					tabBarIcon: ({ color }) => <SearchIcon color={color} />,
 				}}
 			/>
 			<Tabs.Screen
 				name="add"
 				options={{
-					tabBarIcon: ({ color }) => (
-						<PlusIcon color={color} />
-					),
+					tabBarIcon: ({ color }) => <PlusIcon color={color} />,
 				}}
 			/>
 			<Tabs.Screen
 				name="likes"
 				options={{
-					tabBarIcon: ({ color }) => (
-						<HeartIcon color={color} />
-					),
+					tabBarIcon: ({ color }) => <HeartIcon color={color} />,
 				}}
 			/>
 			<Tabs.Screen
@@ -99,7 +84,13 @@ export default function TabLayout() {
 							source={{ uri: "https://i.pravatar.cc/150?img=2" }}
 							className="mt-1 w-8 h-8 rounded-full"
 						/>
-					)
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="user"
+				options={{
+					tabBarIcon: ({ color }) => <UserIcon color={color} />,
 				}}
 			/>
 		</Tabs>
